0a7ab3c72de408ca2d4824a4ce104a540309b2a0
Desi Cochrane <desi@desicochrane.com>
2018-01-31 13:25:44 +0100

    added formulae

---
+++ jnl.rb
@@ -0,0 +1,11 @@
+ class Jnl < Formula
+  desc "Cli accounting tool"
+  homepage "https://github.com/desicochrane/jnl"
+  url "https://github.com/desicochrane/jnl/archive/v0.1.0.tar.gz"
+  sha256 "8fa7d540d9b98e0f44e04d03122f936cb14a84cfb15832f9ff0f99751d01fd7c"
+
+  def install
+    bin.install "jnl"
+  end
+
+end
